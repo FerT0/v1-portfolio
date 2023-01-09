a68b60180b5b75114cbaeb596aaf11d2134f2f65
FerT0 <jtasconacevedo@gmail.com>
2023-01-08 22:53:27 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./app"
+import Home from "./App"
 import "./app.css"
 
 ReactDOM.render(
