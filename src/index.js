da8f54800144f3cb0f43dbb025949fb9eab74262
FerT0 <jtasconacevedo@gmail.com>
2023-01-08 22:49:32 -0500

    Fixed responsive issues

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./App"
+import Home from "./app"
 import "./app.css"
 
 ReactDOM.render(
